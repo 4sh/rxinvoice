b10f44d81641886cd2bb7e6f90bb2c100ed9c698
carmarolli <christophe.armarolli@4sh.fr>
2020-04-03 17:55:00 +0200

    RXINVO-185 [Export TVA] : Stocker la configuration prestations par vendeur

--- ui/src/app/pages/seller-settings/seller-settings.component.ts
+++ ui/src/app/pages/seller-settings/seller-settings.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {SellerSettingsModel} from '../../models/seller-settings.model';
 import {AuthenticationService} from '../../common/services/authentication.service';
+import {ServiceKind} from '../../models/service.kind';
 
 @Component({
     selector: 'seller-settings',
@@ -11,6 +12,9 @@ export class SellerSettingsComponent implements OnInit {
 
     public sellerSettingsModel: SellerSettingsModel;
 
+
+    serviceKinds: Array<ServiceKind> = ['SUBCONTRACTING', 'FEE', 'SERVICE', 'BUY_SELL', 'TRAINING', 'HOSTING'];
+
     constructor(private authService: AuthenticationService) {
     }
 
@@ -20,5 +24,9 @@ export class SellerSettingsComponent implements OnInit {
             .subscribe(company => {
                 this.sellerSettingsModel = company.sellerSettings;
             });
+    }
+
+    getServiceReferences(serviceKind: ServiceKind) {
+        return this.sellerSettingsModel.serviceReferenceList.filter(value => value.kind === serviceKind);
     }
 }
