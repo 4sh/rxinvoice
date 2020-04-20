14ffc69eb062474dca52ae43c0caad8ade56a3aa
carmarolli <christophe.armarolli@4sh.fr>
2020-04-20 23:48:38 +0200

    RXINVO-169 Ecran de gestion de la TVA/prestation
    Create service kind component

--- ui/src/app/models/service.kind.ts
+++ ui/src/app/models/service.kind.ts
@@ -1,1 +1,12 @@
 export type ServiceKind = 'SUBCONTRACTING' | 'FEE' | 'SERVICE' | 'BUY_SELL' | 'TRAINING' | 'HOSTING';
+
+export const SERVICE_KINDS: Array<ServiceKind> = ['SUBCONTRACTING', 'FEE', 'SERVICE', 'BUY_SELL', 'TRAINING', 'HOSTING'];
+
+export enum ServiceKindEnum {
+    SUBCONTRACTING = 'SUBCONTRACTING',
+    FEE = 'FEE',
+    SERVICE = 'SERVICE',
+    BUY_SELL = 'BUY_SELL',
+    TRAINING = 'TRAINING',
+    HOSTING = 'HOSTING',
+}
