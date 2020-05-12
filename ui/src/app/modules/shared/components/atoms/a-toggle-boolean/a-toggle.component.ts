e6484d2189db9e122ba2d8e178e81d3eb32b494f
carmarolli <christophe.armarolli@4sh.fr>
2020-05-12 11:55:13 +0200

    Implement toggle component

--- ui/src/app/modules/shared/components/atoms/a-toggle-boolean/a-toggle.component.ts
+++ ui/src/app/modules/shared/components/atoms/a-toggle-boolean/a-toggle.component.ts
@@ -1,15 +1,50 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {Component, forwardRef, Input} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+
+const VALUE_ACCESSOR = {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => AToggleComponent),
+    multi: true
+};
 
 @Component({
     selector: 'a-toggle',
     templateUrl: './a-toggle.component.html',
-    styleUrls: ['./a-toggle.component.scss']
+    styleUrls: ['./a-toggle.component.scss'],
+    providers: [VALUE_ACCESSOR]
 })
-export class AToggleComponent {
+export class AToggleComponent implements ControlValueAccessor {
 
     @Input() showLabel = true;
     @Input() label: String;
-    @Input() item = false;
     @Input() disabled = false;
-    @Output() itemChange: EventEmitter<boolean> = new EventEmitter();
+
+    public value: Boolean;
+
+    private onNgChange: (value: boolean) => void;
+    private onNgTouched: () => void;
+
+    constructor() {
+    }
+
+    registerOnChange(fn: any): void {
+        this.onNgChange = fn;
+    }
+
+    registerOnTouched(fn: any): void {
+        this.onNgTouched = fn;
+    }
+
+    writeValue(value: boolean): void {
+        this.value = value;
+    }
+
+    setDisabledState(isDisabled: boolean): void {
+        this.disabled = isDisabled;
+    }
+
+    onChange(value: boolean): void {
+        this.value = value;
+        this.onNgChange(value);
+    }
 }
