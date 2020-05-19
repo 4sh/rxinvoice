789ecaf8fad58d7bb7fc166e6cce46823118a690
carmarolli <christophe.armarolli@4sh.fr>
2020-05-19 18:25:01 +0200

    RXINVO-198 Login Redirection on 401 Unauthorized

--- ui/src/app/common/services/http-interceptor.service.ts
+++ ui/src/app/common/services/http-interceptor.service.ts
@@ -1,18 +1,28 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs/internal/Observable';
+import {catchError} from 'rxjs/operators';
+import {throwError} from 'rxjs/internal/observable/throwError';
+import {Router} from '@angular/router';
 
 @Injectable()
 export class HttpInterceptorService implements HttpInterceptor {
 
-    constructor() {};
-
-    intercept(req: HttpRequest< any >, next: HttpHandler): Observable<HttpEvent<any>> {
-        const locale: string = navigator.language;
-        // FIXME issue with LOCALE_ID , 'en-US' for me...?
-
-        req = req.clone({ headers: req.headers.set('Accept-Language', locale) });
-        return next.handle(req);
+    constructor(private router: Router) {
     }
 
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const locale: string = navigator.language;
+        req = req.clone({headers: req.headers.set('Accept-Language', locale)});
+
+        return next
+            .handle(req)
+            .pipe(catchError((error) => {
+                    if (error.status === 401) {
+                        this.router.navigate(['/login']);
+                    }
+                    return throwError(error.message);
+                })
+            );
+    }
 }
