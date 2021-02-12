6cdd1e554c3c11be5f06395fde5f228650e28f02
carmarolli <christophe.armarolli@4sh.fr>
2021-02-12 15:25:42 +0100

    Fix tslint

--- ui/src/app/modules/invoice/invoice.module.ts
+++ ui/src/app/modules/invoice/invoice.module.ts
@@ -15,7 +15,7 @@ import {DownloadInvoiceService} from './services/download-invoice.service';
 import {ButtonsModule} from '../shared/components/atoms/buttons/buttons.module';
 import {InvoiceResolver} from './resolvers/invoice.resolver';
 import {AInvoiceLineComponent} from './components/a-invoice-line/a-invoice-line.component';
-import {CustomerModule} from "../customer/customer.module";
+import {CustomerModule} from '../customer/customer.module';
 import {DndModule} from 'ngx-drag-drop';
 
 const components = [
