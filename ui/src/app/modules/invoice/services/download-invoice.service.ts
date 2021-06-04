994faed90a8963135df477c166dfc585331daccf
paulvigneau <paul.vigneau@4sh.fr>
2021-06-04 09:31:44 +0200

    Print invoice PDF in a new tab

--- ui/src/app/modules/invoice/services/download-invoice.service.ts
+++ ui/src/app/modules/invoice/services/download-invoice.service.ts
@@ -1,11 +1,14 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {catchError, tap} from "rxjs/operators";
+import {throwError} from "rxjs/internal/observable/throwError";
 
 @Injectable()
 export class DownloadInvoiceService {
 
-    private baseUrl = '/api/print';
+    private baseUrl = '/api/v1/print';
 
-    constructor() {
+    constructor(private http: HttpClient) {
     }
 
     openInvoicePdfAsTab(invoice) {
@@ -17,7 +20,16 @@ export class DownloadInvoiceService {
     downloadInvoice(invoice) {
         const url = this.baseUrl + '?invoiceId=' + invoice._id;
         const url2 = '&filename=' + invoice.generatePdfFilename(invoice);
-        window.location.href = url + url2;
+
+        this.http.get(url + url2, {responseType: "arraybuffer", observe: "response"}).pipe(
+            tap((response) => {
+                let file = new Blob([response.body], {type: 'application/pdf'});
+                window.open(URL.createObjectURL(file), '_blank');
+            }),
+            catchError((response: Response) => throwError({
+                message: 'Unable to download pdf',
+                response: response
+            }))).toPromise()
     }
 
 }
