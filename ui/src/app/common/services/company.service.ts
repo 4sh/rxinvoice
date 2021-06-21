a96bcfc191140c8edd978eae940aada3809731dd
paulvigneau <paul.vigneau@4sh.fr>
2021-06-21 09:54:48 +0200

    Fix companyService request

--- ui/src/app/common/services/company.service.ts
+++ ui/src/app/common/services/company.service.ts
@@ -10,14 +10,14 @@ import {throwError} from 'rxjs/internal/observable/throwError';
 @Injectable()
 export class CompanyService {
 
-    private baseUrl = '/api/v1/customers';
+    private baseUrl = '/api/v1/companies';
 
     constructor(private http: HttpClient) {
     }
 
     public fetchCompany(id): Observable<Company> {
         return this.http
-            .get(this.baseUrl + id).pipe(
+            .get(this.baseUrl + '/' + id).pipe(
                 map((result: any) => plainToClass(Company, result as Object)),
                 catchError((response: Response) => throwError({message: 'Unable to fetch company', response: response})));
     }
