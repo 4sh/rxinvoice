3b34150932fd6fb399f314c64b1af1b8a958face
chrhuang <christian.huang.49@gmail.com>
2023-05-25 15:28:32 +0200

    SIMINVOICE-41: Add total in invoices index

--- ui/src/app/modules/invoice/components/invoices-list/invoices-list.component.ts
+++ ui/src/app/modules/invoice/components/invoices-list/invoices-list.component.ts
@@ -54,7 +54,7 @@ export class InvoicesListComponent {
             if (sortParam.includes(ASC)) {
                 this.invoiceService.invoiceSearchFilter.sortParam = attribute + '_' + DESC;
             } else if (sortParam.includes(DESC)) {
-                this.invoiceService.invoiceSearchFilter.sortParam = attribute + '_'+ ASC;
+                this.invoiceService.invoiceSearchFilter.sortParam = attribute + '_' + ASC;
             }
         } else {
             this.invoiceService.invoiceSearchFilter.sortParam = attribute + '_' + ASC;
@@ -75,5 +75,17 @@ export class InvoicesListComponent {
         } else if (this.sortParam.includes(DESC)) {
             return 'down';
         }
+    }
+
+    getTotalGrossAmount(invoices: Array<Invoice>) {
+        return invoices.reduce((acc, invoice) => acc + invoice.grossAmount, 0);
+    }
+
+    getTotalNetAmount(invoices: Array<Invoice>) {
+        return invoices.reduce((acc, invoice) => acc + invoice.netAmount, 0);
+    }
+
+    getAllBuyers(invoices: Array<Invoice>) {
+        return invoices.map(invoice => invoice.buyer.name).filter((value, index, self) => self.indexOf(value) === index).join(', ');
     }
 }
